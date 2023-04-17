dd09a59d29ed435bdd46a6a2fa5501cc60f6446e
Gernanda Mahadlan <gernandamahadlan@gmail.com>
2023-04-17 20:39:36 +0700

    unknown

---
+++ dist/index.4207ec01.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".collapsible").forEach((l=>l.addEventListener("click",(function(){this.classList.toggle("collapsible--expand")}))));
+//# sourceMappingURL=index.4207ec01.js.map
